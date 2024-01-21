e73ebba5211fe068513c055634ad126e283d5ff4
SkorEgor <92719975+SkorEgor@users.noreply.github.com>
2024-01-21 15:25:12 +0300

    Refactoring

--- gui_logic.py
+++ gui_logic.py
@@ -1,66 +1,67 @@
-from gui import Ui_Dialog
+from PyQt5 import QtWidgets
+import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
-import matplotlib
 
-matplotlib.use("Qt5Agg")
+matplotlib.use('TkAgg')
+
+from gui import Ui_Dialog
 
 
-# КЛАСС АЛГОРИТМА ПРИЛОЖЕНИЯ
 class GuiProgram(Ui_Dialog):
+    """ Класс контроллер - интерпретирует действия пользователя """
 
-    def __init__(self, dialog):
+    def __init__(self, dialog: QtWidgets.QDialog) -> None:
+        """ Вызывается при создании нового объекта класса """
+        # Создание окна
+        Ui_Dialog.__init__(self)
+        # Установка пользовательского интерфейс
+        self.setupUi(dialog)
+
         # ПОЛЯ КЛАССА
-        # Параметры 1 графика
-        self.ax1 = None
-        self.fig1 = None
-        self.canvas1 = None
-        self.toolbar1 = None
+        # Параметры 1-ого графика
+        self.ax_1 = None
+        self.fig_1 = None
+        self.canvas_1 = None
+        self.toolbar_1 = None
 
         # ДЕЙСТВИЯ ПРИ ВКЛЮЧЕНИИ
-        # Создаем окно
-        Ui_Dialog.__init__(self)
-        self.setupUi(dialog)  # Устанавливаем пользовательский интерфейс
-
-        # Initialize the figure in our window
-        self.initialize_figure()  # Initialize!
+        # Инициализируем параметры 1-ого графика
+        self.initialize_graph()
 
         # Отрисовка графика
         a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.plotting_without_noise(a, b)
+        self.plotting(a, b)
 
-    def initialize_figure(self, draw=False):
-        # Инициализирует фигуру matplotlib внутри контейнера GUI.
-        # Вызываем только один раз при инициализации
+    def initialize_graph(self) -> None:
+        """ Инициализирует фигуру matplotlib внутри контейнера GUI.
+        Вызываем только один раз для инициализации объектов графика."""
 
         # Создание фигуры (self.fig и self.ax)
-        self.fig1 = Figure()  # Prep empty figure
-        self.ax1 = self.fig1.add_subplot(111)  # Prep empty plot
+        self.fig_1 = Figure()  # Создание пустой фигуры
+        self.ax_1 = self.fig_1.add_subplot(111)  # Место для графика, вид разделения
         # Создание холста
-        self.canvas1 = FigureCanvas(self.fig1)
-        self.plotLayout.addWidget(self.canvas1)
-        if draw:
-            self.canvas1.draw()
-
+        self.canvas_1 = FigureCanvas(self.fig_1)
+        self.plotLayout.addWidget(self.canvas_1)
         # Создание Toolbar
-        self.toolbar1 = NavigationToolbar(self.canvas1, self.plotWindow,
-                                          coordinates=True)
-        self.plotLayout.addWidget(self.toolbar1)
+        self.toolbar_1 = NavigationToolbar(
+            self.canvas_1, self.plotWindow, coordinates=True)
+        self.plotLayout.addWidget(self.toolbar_1)
 
-    def plotting_without_noise(self, data_x, data_y):
+    def plotting(self, data_x, data_y):
         # Очищаем график
-        self.ax1.clear()
+        self.ax_1.clear()
         # Задаем название осей
-        self.ax1.set_xlabel('frequency')
-        self.ax1.set_ylabel('gamma')
+        self.ax_1.set_xlabel('x')  # Необязательно
+        self.ax_1.set_ylabel('y')  # Необязательно
 
         # Строим график
-        self.ax1.plot(data_x, data_y, color='r', label='empty')
+        self.ax_1.plot(data_x, data_y, color='r')
 
         # Убеждаемся, что все помещается внутри холста
-        self.fig1.tight_layout()
+        self.fig_1.tight_layout()
         # Показываем новую фигуру в интерфейсе
-        self.canvas1.draw()
+        self.canvas_1.draw()
